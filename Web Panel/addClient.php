631ce633fde364e916c462c1655eb774755a723a
Mahmoud Hamad <dr.mah2021@gmail.com>
2023-11-01 18:03:32 +0200

    Update addClient.php
    add comments

--- Web Panel/addClient.php
+++ Web Panel/addClient.php
@@ -1,21 +1,31 @@
 <?php
 
- $db = new mysqli('localhost', 'root','', 'controlserver');
- if(mysqli_connect_errno()) {
-      
-    exit;
- }
-	
+// Establish a connection to the MySQL database
+$db = new mysqli('localhost', 'root', '', 'controlserver');
 
-$name = $_POST["name"];
-$ip = $_POST["ip"];
-$id = $_POST["id"];
-$os = $_POST["os"];
-$country = $_POST["country"];
+// Check for connection errors
+if (mysqli_connect_errno()) {
+    // If there's an error in the database connection, exit the script
+    exit;
+}
+
+// Retrieve POST data sent from an external source (e.g., a client-side application or system)
+$name = $_POST["name"];     // Name of the client
+$ip = $_POST["ip"];         // IP address of the client
+$id = $_POST["id"];         // Unique ID of the client
+$os = $_POST["os"];         // Operating system information of the client
+$country = $_POST["country"]; // Country information of the client
+
+// Prepare an SQL INSERT statement to insert data into the 'clients' table
 $query = "INSERT INTO clients(name, ip, id, os, country) VALUES(?,?,?,?,?)";
 $stmt = $db->prepare($query);
-$stmt->bind_param('sssss',$name,$ip,$id,$os,$country);
-$stmt->execute();
-$db->close();
 
+// Bind the received POST data to the SQL query using 'bind_param' to prevent SQL injection
+$stmt->bind_param('sssss', $name, $ip, $id, $os, $country);
+
+// Execute the SQL statement to insert the provided information into the database
+$stmt->execute();
+
+// Close the database connection after the insertion operation
+$db->close();
 ?>
